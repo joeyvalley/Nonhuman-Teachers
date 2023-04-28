5ac2d4a408c3cc53847c02d0438d1d65d88caa5f
Joseph Valle <josephrvalle@gmail.com>
2023-04-28 00:26:37 -0400

    added videos and changed vertical alignment

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,10 @@ function App() {
     { title: "Green Stentor coeruleus and Vorticella at 25x magnification. Video by Frank Fox.", link: "https://i.imgur.com/ENiRikD.mp4" },
     { title: "Perspiration on a human fingertip at 5 - 40x magnification. Video by Tsutomu Tomita and Shun Miyazaki.", link: "https://i.imgur.com/KY947KI.mp4" },
     { title: "A ciliate from genus Pseudomicrothorax devours a cyanobacterial filament at 20x and 40x magnification. Video by Dr. Sally Warring.", link: "https://i.imgur.com/cTQqlEk.mp4" },
+    { title: "Oil droplets on a soap film at 4x magnification. Video by Karl Gaff.", link: "https://i.imgur.com/1OXq0sE.mp4" },
+    { title: "Formation of silver dendrites at 3x magnification. Video by Wojtek Plonka.", link: "https://i.imgur.com/xrukDYR.mp4" },
+    { title: "The rotifer Limnias melicerta at 200x magnification. Video by Wim van Egmond.", link: "https://i.imgur.com/iIntJym.mp4" },
+    { title: "5-day time-lapse of rat hippocampal neurons at 20x magnification. Video by Thomas A. Zangle and Soorya Pradeep.", link: "https://i.imgur.com/FDOon9v.mp4" },
     { title: "Polychaete worm of the Syllidae family at 20x and 40x magnification. Video by Rafael Martín-Ledo.", link: "https://i.imgur.com/R4UIzum.mp4" }
   ]
   const startingVideo = randomVideo();
