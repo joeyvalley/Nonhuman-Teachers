4db501bd55dd74b4a2dbe4e5c00a272ae116c9a7
Joseph Valle <josephrvalle@gmail.com>
2023-06-21 14:48:06 -0400

    fixed the routing for going live with the donate page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Landing from './pages/Landing'
 import Donate from './pages/Donate'
 // import Landing from './pages/Landing'
 
@@ -7,7 +8,7 @@ export default function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Landing />}></Route>
         <Route path="/donate/" element={<Donate />}></Route>
       </Routes>
     </>
