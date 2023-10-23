e341568b2b92a7ba3f0f579cd2c12b7d4d286ce9
Joseph Valle <josephrvalle@gmail.com>
2023-10-23 17:30:40 -0400

    going back to the serverless function for newsletter

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,33 +1,33 @@
 import { useState } from "react";
-// import axios from 'axios';
+import axios from 'axios';
 import "../styles/newsletter.css"
-import signUp from "../api/subscribe";
 
 export default function Newsletter() {
-  const [email, setEmail] = useState("");
+  const [email, setEmail] = useState();
 
   async function handleSubmit(e) {
     e.preventDefault();
     const email = e.target.EMAIL.value; // Get email value from the form
-    const res = await signUp(email);
+    try {
+      const response = await axios.post('/api/subscribe', { email });
+      console.log(response.status);
 
-    if (res === "subscribed") {
-      setEmail("Thanks for subscribing! Talk soon : )");
-    } else if (res === "exists") {
-      setEmail("You are already subscribed! Thanks tho : )")
+      if (response.status === 200) {
+        setEmail("Thank you for subscribing!")
+      } else {
+        setEmail("An error occurred, please try again.")
+        console.log('An error occurred:', response);
+      }
+    } catch (error) {
+      setEmail("Whoops.")
+      console.log('An error occurred:', error);
     }
-    else {
-      setEmail("Sorry, there was an error. Please try again!")
-    }
-    setTimeout(() => {
-      setEmail("E-mail")
-    }, 5000);
   }
 
   return (
     <div className="newsletter">
       <form onSubmit={handleSubmit}>
-        <input type="email" name="EMAIL" className="email" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        <input type="email" name="EMAIL" className="email" id="mce-EMAIL" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
         <input type="submit" value="Subscribe" className="subscribe" />
       </form>
     </div>
