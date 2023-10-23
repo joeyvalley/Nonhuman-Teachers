60f640cc99619e7819b0c214b844c2508ffb7d66
Joseph Valle <josephrvalle@gmail.com>
2023-10-23 18:20:11 -0400

    hopefully this finishes it

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,36 +1,35 @@
 import { useState } from "react";
-import axios from 'axios';
-import "../styles/newsletter.css"
+import axios from "axios";
+import "../styles/newsletter.css";
 
 export default function Newsletter() {
-  const [email, setEmail] = useState();
+  const [email, setEmail] = useState("");
 
   async function handleSubmit(e) {
     e.preventDefault();
-    const email = e.target.EMAIL.value; // Get email value from the form
+    const enteredEmail = e.target.EMAIL.value; // Get email value from the form
     try {
-      const response = await axios.post('/api/subscribe', { email });
-      console.log(response.status);
-
-      if (response.status === 200) {
-        setEmail("Thank you for subscribing!")
+      const { status, data } = await axios.post('/api/subscribe', { email: enteredEmail });
+      console.log(status);
+      if (status === 200) {
+        setEmail("Thanks for subscribing! Talk soon.");
+      } else if (status === 400) {
+        setEmail("You are already subscribed to this newsletter. Thanks!");
       } else {
-        setEmail("An error occurred, please try again.")
-        console.log('An error occurred:', response);
+        setEmail("Whoops, an unexpected error occurred. Try again later.");
       }
     } catch (error) {
-      if (error.status === 400) {
-        console.log(error.response.data.title);
-      }
+      setEmail("Whoops, an error occurred. Try again later.");
+      console.log('An error occurred:', error);
     }
   }
 
   return (
     <div className="newsletter">
       <form onSubmit={handleSubmit}>
-        <input type="email" name="EMAIL" className="email" id="mce-EMAIL" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        <input type="email" name="EMAIL" className="email" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} required />
         <input type="submit" value="Subscribe" className="subscribe" />
       </form>
     </div>
-  )
+  );
 }
